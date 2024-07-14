727320b32c5a2b0c584eb34c26ef22cd2135695f
bjosk <bjornar.skar@hotmail.com>
2024-07-14 15:57:15 +0200

    changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - TESTing5" );
+        System.out.println( "Hello World! - TESTing6" );
     }
 }
